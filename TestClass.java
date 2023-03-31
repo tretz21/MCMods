c0e3a2aa4841e139d1ca3a8ffe674cffe03f6247
tretz <thomascwretz@gmail.com>
2023-03-31 01:20:17 -0400

    second commit

--- TestClass.java
+++ TestClass.java
@@ -1,5 +1,7 @@
 public class TestClass {
     public static void main(String[] args){
         System.out.println("Hello World!");
+        int dog = 1;
+        System.out.println(Integer.toString(dog) + 1);
     }
 }
